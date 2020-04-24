72a86fcd25e8b7b2ab32cbd58895c26232c60fca
stoianandrei9090 <stoianandrei@yahoo.com>
2020-04-24 16:42:32 +0300

    myAtoi done but not tested

--- src/mainpackage/MyAtoi.java
+++ src/mainpackage/MyAtoi.java
@@ -4,21 +4,23 @@ public class MyAtoi {
 
     public static int myAtoi(String str) {
         str = str.trim();
-        int sign = (str.startsWith("-")) ? -1 : 1;
+        if(str.matches("[^-+\\d].*")) return 0;
+        double sign = (str.startsWith("-")) ? -1 : 1;
         str = str.replaceAll("\\D", "");
-        int result = 0;
+        double result = 0d;
         if(str.equals("") || str.equals("0")) return 0;
         else {
-
             for(int i = 0 ; i<str.length(); i++) {
                  result = result * 10 + ((str.charAt(i) - 48));
             }
         }
-        return sign*result;
-
+        double end = sign*result;
+        if(end < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        else if (end > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        else return (int) end;
     }
 
     public static void main(String[] args) {
-        System.out.println(myAtoi("    -23 with words"));
+        System.out.println(myAtoi("-9999999999999999999999999"));
     }
 }
